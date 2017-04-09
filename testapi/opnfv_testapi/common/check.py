opnfv/releng

unify data existence check
Change-Id: I2ee4c3be6f34ce12530450cd22f2561c458685f9
Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

utils/test/testapi/opnfv_testapi/common/check.py
@@ -1,0 +1,112 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corp
+# feng.xiaowei@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import functools
+
+from tornado import web, gen
+
+from opnfv_testapi.common import raises, message
+
+
+def authenticate(method):
+    @web.asynchronous
+    @gen.coroutine
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if self.auth:
+            try:
+                token = self.request.headers['X-Auth-Token']
+            except KeyError:
+                raises.Unauthorized(message.unauthorized())
+            query = {'access_token': token}
+            check = yield self._eval_db_find_one(query, 'tokens')
+            if not check:
+                raises.Forbidden(message.invalid_token())
+        ret = yield gen.coroutine(method)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrapper
+
+
+def not_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        query = kwargs.get('query')
+        data = yield self._eval_db_find_one(query)
+        if not data:
+            raises.NotFound(message.not_found(self.table, query))
+        ret = yield gen.coroutine(xstep)(self, data, *args, **kwargs)
+        raise gen.Return(ret)
+
+    return wrap
+
+
+def no_body(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        if self.json_args is None:
+            raises.BadRequest(message.no_body())
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+
+    return wrap
+
+
+def miss(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        miss_checks = kwargs.get('miss_checks')
+        if miss_checks:
+            for miss in miss_checks:
+                miss_data = self.json_args.get(miss)
+                if miss_data is None or miss_data == '':
+                    raises.BadRequest(message.missing(miss))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def carriers_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        carriers = kwargs.get('carriers')
+        if carriers:
+            for table, query in carriers:
+                exist = yield self._eval_db_find_one(query(), table)
+                if not exist:
+                    raises.Forbidden(message.not_found(table, query()))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def new_not_exists(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        query = kwargs.get('query')
+        if query:
+            to_data = yield self._eval_db_find_one(query())
+            if to_data:
+                raises.Forbidden(message.exist(self.table, query()))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def updated_one_not_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, data, *args, **kwargs):
+        db_keys = kwargs.get('db_keys')
+        query = self._update_query(db_keys, data)
+        if query:
+            to_data = yield self._eval_db_find_one(query)
+            if to_data:
+                raises.Forbidden(message.exist(self.table, query))
+        ret = yield gen.coroutine(xstep)(self, data, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+

utils/test/testapi/opnfv_testapi/resources/scenario_handlers.py
@@ -1,5 +1,4 @@
 import functools
-import httplib
 
 from opnfv_testapi.common import message
 from opnfv_testapi.common import raises
@@ -65,7 +64,7 @@ class ScenariosCLHandler(GenericScenarioHandler):
                 query['installers'] = {'$elemMatch': elem_query}
             return query
 
-        self._list(_set_query())
+        self._list(query=_set_query())
 
     @swagger.operation(nickname="createScenario")
     def post(self):
@@ -79,15 +78,10 @@ class ScenariosCLHandler(GenericScenarioHandler):
             @raise 403: scenario already exists
             @raise 400:  body or name not provided
         """
-        def query(data):
-            return {'name': data.name}
-
-        def error(data):
-            return httplib.FORBIDDEN, message.exist('scenario', data.name)
-
+        def query():
+            return {'name': self.json_args.get('name')}
         miss_checks = ['name']
-        db_checks = [(self.table, False, query, error)]
-        self._create(miss_checks=miss_checks, db_checks=db_checks)
+        self._create(miss_checks=miss_checks, query=query)
 
 
 class ScenarioGURHandler(GenericScenarioHandler):
@@ -99,7 +93,7 @@ class ScenarioGURHandler(GenericScenarioHandler):
             @return 200: scenario exist
             @raise 404: scenario not exist
         """
-        self._get_one({'name': name})
+        self._get_one(query={'name': name})
         pass
 
     @swagger.operation(nickname="updateScenarioByName")
@@ -116,7 +110,7 @@ class ScenarioGURHandler(GenericScenarioHandler):
         """
         query = {'name': name}
         db_keys = ['name']
-        self._update(query, db_keys)
+        self._update(query=query, db_keys=db_keys)
 
     @swagger.operation(nickname="deleteScenarioByName")
     def delete(self, name):
@@ -126,19 +120,16 @@ class ScenarioGURHandler(GenericScenarioHandler):
         @raise 404: scenario not exist:
         """
 
-        query = {'name': name}
-        self._delete(query)
+        self._delete(query={'name': name})
 
     def _update_query(self, keys, data):
         query = dict()
-        equal = True
         if self._is_rename():
             new = self._term.get('name')
-            if data.name != new:
-                equal = False
+            if data.get('name') != new:
                 query['name'] = new
 
-        return equal, query
+        return query
 
     def _update_requests(self, data):
         updates = {

utils/test/testapi/opnfv_testapi/resources/testcase_handlers.py
@@ -6,9 +6,7 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-import httplib
 
-from opnfv_testapi.common import message
 from opnfv_testapi.resources import handlers
 from opnfv_testapi.resources import testcase_models
 from opnfv_testapi.tornado_swagger import swagger
@@ -32,9 +30,7 @@ class TestcaseCLHandler(GenericTestcaseHandler):
                          empty list is no testcase exist in this project
             @rtype: L{TestCases}
         """
-        query = dict()
-        query['project_name'] = project_name
-        self._list(query)
+        self._list(query={'project_name': project_name})
 
     @swagger.operation(nickname="createTestCase")
     def post(self, project_name):
@@ -49,26 +45,18 @@ class TestcaseCLHandler(GenericTestcaseHandler):
                         or testcase already exists in this project
             @raise 400: body or name not provided
         """
-        def p_query(data):
-            return {'name': data.project_name}
+        def project_query():
+            return {'name': project_name}
 
-        def tc_query(data):
-            return {
-                'project_name': data.project_name,
-                'name': data.name
-            }
-
-        def p_error(data):
-            return httplib.FORBIDDEN, message.not_found('project',
-                                                        data.project_name)
-
-        def tc_error(data):
-            return httplib.FORBIDDEN, message.exist('testcase', data.name)
-
+        def testcase_query():
+            return {'project_name': project_name,
+                    'name': self.json_args.get('name')}
         miss_checks = ['name']
-        db_checks = [(self.db_projects, True, p_query, p_error),
-                     (self.db_testcases, False, tc_query, tc_error)]
-        self._create(miss_checks, db_checks, project_name=project_name)
+        carriers = [(self.db_projects, project_query)]
+        self._create(miss_checks=miss_checks,
+                     carriers=carriers,
+                     query=testcase_query,
+                     project_name=project_name)
 
 
 class TestcaseGURHandler(GenericTestcaseHandler):
@@ -84,7 +72,7 @@ class TestcaseGURHandler(GenericTestcaseHandler):
         query = dict()
         query['project_name'] = project_name
         query["name"] = case_name
-        self._get_one(query)
+        self._get_one(query=query)
 
     @swagger.operation(nickname="updateTestCaseByName")
     def put(self, project_name, case_name):
@@ -102,7 +90,7 @@ class TestcaseGURHandler(GenericTestcaseHandler):
         """
         query = {'project_name': project_name, 'name': case_name}
         db_keys = ['name', 'project_name']
-        self._update(query, db_keys)
+        self._update(query=query, db_keys=db_keys)
 
     @swagger.operation(nickname='deleteTestCaseByName')
     def delete(self, project_name, case_name):
@@ -112,4 +100,4 @@ class TestcaseGURHandler(GenericTestcaseHandler):
             @raise 404: testcase not exist
         """
         query = {'project_name': project_name, 'name': case_name}
-        self._delete(query)
+        self._delete(query=query)

rohitsakala/releng-testresults

testapi/opnfv_testapi/common/check.py
@@ -1,0 +1,111 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corp
+# feng.xiaowei@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import functools
+
+from tornado import web, gen
+
+from opnfv_testapi.common import raises, message
+
+
+def authenticate(method):
+    @web.asynchronous
+    @gen.coroutine
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if self.auth:
+            try:
+                token = self.request.headers['X-Auth-Token']
+            except KeyError:
+                raises.Unauthorized(message.unauthorized())
+            query = {'access_token': token}
+            check = yield self._eval_db_find_one(query, 'tokens')
+            if not check:
+                raises.Forbidden(message.invalid_token())
+        ret = yield gen.coroutine(method)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrapper
+
+
+def not_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        query = kwargs.get('query')
+        data = yield self._eval_db_find_one(query)
+        if not data:
+            raises.NotFound(message.not_found(self.table, query))
+        ret = yield gen.coroutine(xstep)(self, data, *args, **kwargs)
+        raise gen.Return(ret)
+
+    return wrap
+
+
+def no_body(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        if self.json_args is None:
+            raises.BadRequest(message.no_body())
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+
+    return wrap
+
+
+def miss_fields(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        fields = kwargs.get('miss_fields')
+        if fields:
+            for miss in fields:
+                miss_data = self.json_args.get(miss)
+                if miss_data is None or miss_data == '':
+                    raises.BadRequest(message.missing(miss))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def carriers_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        carriers = kwargs.get('carriers')
+        if carriers:
+            for table, query in carriers:
+                exist = yield self._eval_db_find_one(query(), table)
+                if not exist:
+                    raises.Forbidden(message.not_found(table, query()))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def new_not_exists(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, *args, **kwargs):
+        query = kwargs.get('query')
+        if query:
+            to_data = yield self._eval_db_find_one(query())
+            if to_data:
+                raises.Forbidden(message.exist(self.table, query()))
+        ret = yield gen.coroutine(xstep)(self, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap
+
+
+def updated_one_not_exist(xstep):
+    @functools.wraps(xstep)
+    def wrap(self, data, *args, **kwargs):
+        db_keys = kwargs.get('db_keys')
+        query = self._update_query(db_keys, data)
+        if query:
+            to_data = yield self._eval_db_find_one(query)
+            if to_data:
+                raises.Forbidden(message.exist(self.table, query))
+        ret = yield gen.coroutine(xstep)(self, data, *args, **kwargs)
+        raise gen.Return(ret)
+    return wrap

testapi/opnfv_testapi/resources/scenario_handlers.py
@@ -1,5 +1,4 @@
 import functools
-import httplib
 
 from opnfv_testapi.common import message
 from opnfv_testapi.common import raises
@@ -65,7 +64,7 @@ class ScenariosCLHandler(GenericScenarioHandler):
                 query['installers'] = {'$elemMatch': elem_query}
             return query
 
-        self._list(_set_query())
+        self._list(query=_set_query())
 
     @swagger.operation(nickname="createScenario")
     def post(self):
@@ -79,15 +78,10 @@ class ScenariosCLHandler(GenericScenarioHandler):
             @raise 403: scenario already exists
             @raise 400:  body or name not provided
         """
-        def query(data):
-            return {'name': data.name}
-
-        def error(data):
-            return httplib.FORBIDDEN, message.exist('scenario', data.name)
-
-        miss_checks = ['name']
-        db_checks = [(self.table, False, query, error)]
-        self._create(miss_checks=miss_checks, db_checks=db_checks)
+        def query():
+            return {'name': self.json_args.get('name')}
+        miss_fields = ['name']
+        self._create(miss_fields=miss_fields, query=query)
 
 
 class ScenarioGURHandler(GenericScenarioHandler):
@@ -99,7 +93,7 @@ class ScenarioGURHandler(GenericScenarioHandler):
             @return 200: scenario exist
             @raise 404: scenario not exist
         """
-        self._get_one({'name': name})
+        self._get_one(query={'name': name})
         pass
 
     @swagger.operation(nickname="updateScenarioByName")
@@ -116,7 +110,7 @@ class ScenarioGURHandler(GenericScenarioHandler):
         """
         query = {'name': name}
         db_keys = ['name']
-        self._update(query, db_keys)
+        self._update(query=query, db_keys=db_keys)
 
     @swagger.operation(nickname="deleteScenarioByName")
     def delete(self, name):
@@ -126,19 +120,16 @@ class ScenarioGURHandler(GenericScenarioHandler):
         @raise 404: scenario not exist:
         """
 
-        query = {'name': name}
-        self._delete(query)
+        self._delete(query={'name': name})
 
     def _update_query(self, keys, data):
         query = dict()
-        equal = True
         if self._is_rename():
             new = self._term.get('name')
-            if data.name != new:
-                equal = False
+            if data.get('name') != new:
                 query['name'] = new
 
-        return equal, query
+        return query
 
     def _update_requests(self, data):
         updates = {

testapi/opnfv_testapi/resources/testcase_handlers.py
@@ -6,9 +6,7 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-import httplib
 
-from opnfv_testapi.common import message
 from opnfv_testapi.resources import handlers
 from opnfv_testapi.resources import testcase_models
 from opnfv_testapi.tornado_swagger import swagger
@@ -32,9 +30,7 @@ class TestcaseCLHandler(GenericTestcaseHandler):
                          empty list is no testcase exist in this project
             @rtype: L{TestCases}
         """
-        query = dict()
-        query['project_name'] = project_name
-        self._list(query)
+        self._list(query={'project_name': project_name})
 
     @swagger.operation(nickname="createTestCase")
     def post(self, project_name):
@@ -49,26 +45,18 @@ class TestcaseCLHandler(GenericTestcaseHandler):
                         or testcase already exists in this project
             @raise 400: body or name not provided
         """
-        def p_query(data):
-            return {'name': data.project_name}
+        def project_query():
+            return {'name': project_name}
 
-        def tc_query(data):
-            return {
-                'project_name': data.project_name,
-                'name': data.name
-            }
-
-        def p_error(data):
-            return httplib.FORBIDDEN, message.not_found('project',
-                                                        data.project_name)
-
-        def tc_error(data):
-            return httplib.FORBIDDEN, message.exist('testcase', data.name)
-
-        miss_checks = ['name']
-        db_checks = [(self.db_projects, True, p_query, p_error),
-                     (self.db_testcases, False, tc_query, tc_error)]
-        self._create(miss_checks, db_checks, project_name=project_name)
+        def testcase_query():
+            return {'project_name': project_name,
+                    'name': self.json_args.get('name')}
+        miss_fields = ['name']
+        carriers = [(self.db_projects, project_query)]
+        self._create(miss_fields=miss_fields,
+                     carriers=carriers,
+                     query=testcase_query,
+                     project_name=project_name)
 
 
 class TestcaseGURHandler(GenericTestcaseHandler):
@@ -84,7 +72,7 @@ class TestcaseGURHandler(GenericTestcaseHandler):
         query = dict()
         query['project_name'] = project_name
         query["name"] = case_name
-        self._get_one(query)
+        self._get_one(query=query)
 
     @swagger.operation(nickname="updateTestCaseByName")
     def put(self, project_name, case_name):
@@ -102,7 +90,7 @@ class TestcaseGURHandler(GenericTestcaseHandler):
         """
         query = {'project_name': project_name, 'name': case_name}
         db_keys = ['name', 'project_name']
-        self._update(query, db_keys)
+        self._update(query=query, db_keys=db_keys)
 
     @swagger.operation(nickname='deleteTestCaseByName')
     def delete(self, project_name, case_name):
@@ -112,4 +100,4 @@ class TestcaseGURHandler(GenericTestcaseHandler):
             @raise 404: testcase not exist
         """
         query = {'project_name': project_name, 'name': case_name}
-        self._delete(query)
+        self._delete(query=query)
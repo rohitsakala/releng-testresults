opnfv/releng

add qtip reporting
Change-Id: Ie573486080fbd36857ff0709d53f6bdcdd43363e
Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

utils/test/reporting/qtip/reporting-status.py
@@ -1,0 +1,110 @@
+#!/usr/bin/python
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+import datetime
+import os
+
+import jinja2
+import utils.reporting_utils as rp_utils
+import utils.scenarioResult as sr
+
+installers = rp_utils.get_config('general.installers')
+versions = rp_utils.get_config('general.versions')
+PERIOD = rp_utils.get_config('general.period')
+
+# Logger
+logger = rp_utils.getLogger("Qtip-Status")
+reportingDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+logger.info("*******************************************")
+logger.info("*   Generating reporting scenario status  *")
+logger.info("*   Data retention = %s days              *" % PERIOD)
+logger.info("*                                         *")
+logger.info("*******************************************")
+
+
+def prepare_profile_file(version):
+    profile_dir = './display/{}/qtip'.format(version)
+    if not os.path.exists(profile_dir):
+        os.makedirs(profile_dir)
+
+    profile_file = '{}/scenario_history.txt'.format(profile_dir, version)
+    if not os.path.exists(profile_file):
+        with open(profile_file, 'w') as f:
+            info = 'date,scenario,installer,details,score\n'
+            f.write(info)
+            f.close()
+    return profile_file
+
+
+def profile_results(results, installer, profile_fd):
+    result_criterias = {}
+    for s_p, s_p_result in results.iteritems():
+        ten_criteria = len(s_p_result)
+        ten_score = sum(s_p_result)
+
+        LASTEST_TESTS = rp_utils.get_config(
+            'general.nb_iteration_tests_success_criteria')
+        four_result = s_p_result[:LASTEST_TESTS]
+        four_criteria = len(four_result)
+        four_score = sum(four_result)
+
+        s_four_score = str(four_score / four_criteria)
+        s_ten_score = str(ten_score / ten_criteria)
+
+        info = '{},{},{},{},{}\n'.format(reportingDate,
+                                         s_p,
+                                         installer,
+                                         s_ten_score,
+                                         s_four_score)
+        profile_fd.write(info)
+        result_criterias[s_p] = sr.ScenarioResult('OK',
+                                                  s_four_score,
+                                                  s_ten_score,
+                                                  '100')
+
+        logger.info("--------------------------")
+    return result_criterias
+
+
+def render_html(result_criterias, installer, version):
+    template_loader = jinja2.FileSystemLoader(".")
+    template_env = jinja2.Environment(loader=template_loader,
+                                      autoescape=True)
+
+    template_file = "./qtip/template/index-status-tmpl.html"
+    template = template_env.get_template(template_file)
+
+    render_outcome = template.render(scenario_results=result_criterias,
+                                     installer=installer,
+                                     period=PERIOD,
+                                     version=version,
+                                     date=reportingDate)
+
+    with open('./display/{}/qtip/status-{}.html'.format(version, installer),
+              'wb') as fh:
+        fh.write(render_outcome)
+
+
+def render_reporter():
+    for version in versions:
+        profile_file = prepare_profile_file(version)
+        profile_fd = open(profile_file, 'a')
+        for installer in installers:
+            results = rp_utils.getQtipResults(version, installer)
+            result_criterias = profile_results(results, installer, profile_fd)
+            render_html(result_criterias=result_criterias,
+                        installer=installer,
+                        version=version)
+        profile_fd.close()
+        logger.info("Manage export CSV")
+        rp_utils.generate_csv(profile_file)
+        logger.error("CSV generated...")
+
+if __name__ == '__main__':
+    render_reporter()

utils/test/reporting/utils/reporting_utils.py
@@ -198,6 +198,36 @@ def getScenarioStatus(installer, version):
     return result_dict
 
 
+def getQtipResults(installer, version):
+    period = get_config('qtip.period')
+    url_base = get_config('testapi.url')
+
+    url = ("http://" + url_base + "?project=qtip" +
+           "&installer=" + installer +
+           "&version=" + version + "&period=" + str(period))
+    request = Request(url)
+
+    try:
+        response = urlopen(request)
+        k = response.read()
+        response.close()
+        results = json.loads(k)
+        test_results = results['results']
+    except URLError as e:
+        print('Got an error code:', e)
+
+    result_dict = {}
+    if test_results:
+        for r in test_results:
+            key = '{}/{}'.format(r['pod_name'], r['scenario'])
+            if key not in result_dict.keys():
+                result_dict[key] = []
+            result_dict[key].append(r['details']['score'])
+
+    # return scenario_results
+    return result_dict
+
+
 def getNbtestOk(results):
     nb_test_ok = 0
     for r in results:
@@ -365,6 +395,14 @@ def export_csv(scenario_file_name, installer, version):
             if installer in line:
                 scenario_installer_file.write(line)
         scenario_installer_file.close
+
+
+def generate_csv(scenario_file):
+    import shutil
+    # csv
+    # generate sub files based on scenario_history.txt
+    csv_file = scenario_file.replace('txt', 'csv')
+    shutil.copy2(scenario_file, csv_file)
 
 
 def export_pdf(pdf_path, pdf_doc_name):

rohitsakala/releng-testresults

reporting/qtip/reporting-status.py
@@ -1,0 +1,110 @@
+#!/usr/bin/python
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+import datetime
+import os
+
+import jinja2
+import utils.reporting_utils as rp_utils
+import utils.scenarioResult as sr
+
+installers = rp_utils.get_config('general.installers')
+versions = rp_utils.get_config('general.versions')
+PERIOD = rp_utils.get_config('general.period')
+
+# Logger
+logger = rp_utils.getLogger("Qtip-Status")
+reportingDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+logger.info("*******************************************")
+logger.info("*   Generating reporting scenario status  *")
+logger.info("*   Data retention = %s days              *" % PERIOD)
+logger.info("*                                         *")
+logger.info("*******************************************")
+
+
+def prepare_profile_file(version):
+    profile_dir = './display/{}/qtip'.format(version)
+    if not os.path.exists(profile_dir):
+        os.makedirs(profile_dir)
+
+    profile_file = '{}/scenario_history.txt'.format(profile_dir, version)
+    if not os.path.exists(profile_file):
+        with open(profile_file, 'w') as f:
+            info = 'date,scenario,installer,details,score\n'
+            f.write(info)
+            f.close()
+    return profile_file
+
+
+def profile_results(results, installer, profile_fd):
+    result_criterias = {}
+    for s_p, s_p_result in results.iteritems():
+        ten_criteria = len(s_p_result)
+        ten_score = sum(s_p_result)
+
+        LASTEST_TESTS = rp_utils.get_config(
+            'general.nb_iteration_tests_success_criteria')
+        four_result = s_p_result[:LASTEST_TESTS]
+        four_criteria = len(four_result)
+        four_score = sum(four_result)
+
+        s_four_score = str(four_score / four_criteria)
+        s_ten_score = str(ten_score / ten_criteria)
+
+        info = '{},{},{},{},{}\n'.format(reportingDate,
+                                         s_p,
+                                         installer,
+                                         s_ten_score,
+                                         s_four_score)
+        profile_fd.write(info)
+        result_criterias[s_p] = sr.ScenarioResult('OK',
+                                                  s_four_score,
+                                                  s_ten_score,
+                                                  '100')
+
+        logger.info("--------------------------")
+    return result_criterias
+
+
+def render_html(prof_results, installer, version):
+    template_loader = jinja2.FileSystemLoader(".")
+    template_env = jinja2.Environment(loader=template_loader,
+                                      autoescape=True)
+
+    template_file = "./qtip/template/index-status-tmpl.html"
+    template = template_env.get_template(template_file)
+
+    render_outcome = template.render(prof_results=prof_results,
+                                     installer=installer,
+                                     period=PERIOD,
+                                     version=version,
+                                     date=reportingDate)
+
+    with open('./display/{}/qtip/status-{}.html'.format(version, installer),
+              'wb') as fh:
+        fh.write(render_outcome)
+
+
+def render_reporter():
+    for version in versions:
+        profile_file = prepare_profile_file(version)
+        profile_fd = open(profile_file, 'a')
+        for installer in installers:
+            results = rp_utils.getQtipResults(version, installer)
+            prof_results = profile_results(results, installer, profile_fd)
+            render_html(prof_results=prof_results,
+                        installer=installer,
+                        version=version)
+        profile_fd.close()
+        logger.info("Manage export CSV")
+        rp_utils.generate_csv(profile_file)
+        logger.info("CSV generated...")
+
+if __name__ == '__main__':
+    render_reporter()

reporting/utils/reporting_utils.py
@@ -198,6 +198,35 @@ def getScenarioStatus(installer, version):
     return result_dict
 
 
+def getQtipResults(version, installer):
+    period = get_config('qtip.period')
+    url_base = get_config('testapi.url')
+
+    url = ("http://" + url_base + "?project=qtip" +
+           "&installer=" + installer +
+           "&version=" + version + "&period=" + str(period))
+    request = Request(url)
+
+    try:
+        response = urlopen(request)
+        k = response.read()
+        response.close()
+        results = json.loads(k)['results']
+    except URLError as e:
+        print('Got an error code:', e)
+
+    result_dict = {}
+    if results:
+        for r in results:
+            key = '{}/{}'.format(r['pod_name'], r['scenario'])
+            if key not in result_dict.keys():
+                result_dict[key] = []
+            result_dict[key].append(r['details']['score'])
+
+    # return scenario_results
+    return result_dict
+
+
 def getNbtestOk(results):
     nb_test_ok = 0
     for r in results:
@@ -365,6 +394,14 @@ def export_csv(scenario_file_name, installer, version):
             if installer in line:
                 scenario_installer_file.write(line)
         scenario_installer_file.close
+
+
+def generate_csv(scenario_file):
+    import shutil
+    # csv
+    # generate sub files based on scenario_history.txt
+    csv_file = scenario_file.replace('txt', 'csv')
+    shutil.copy2(scenario_file, csv_file)
 
 
 def export_pdf(pdf_path, pdf_doc_name):
opnfv/releng

separate db methods from handler.py
db methods are mingled in handler, which is not well structured

Change-Id: I639679d3fc05a0b6528158186b8bf89e0cd10596
Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

utils/test/testapi/opnfv_testapi/ui/auth/sign.py
@@ -1,10 +1,9 @@
 from six.moves.urllib import parse
 
 from opnfv_testapi.common.config import CONF
+from opnfv_testapi.db import api as dbapi
 from opnfv_testapi.ui.auth import base
 from opnfv_testapi.ui.auth import constants as const
-
-# CONF = config.Config()
 
 
 class SigninHandler(base.BaseHandler):
@@ -42,7 +41,7 @@ class SigninReturnHandler(base.BaseHandler):
             'fullname': self.get_query_argument(const.OPENID_NS_SREG_FULLNAME)
         }
 
-        self.db_save(self.table, user_info)
+        dbapi.db_save(self.table, user_info)
         if not self.get_secure_cookie('openid'):
             self.set_secure_cookie('openid', openid)
         self.redirect(url=CONF.ui_url)

rohitsakala/releng-testresults

testapi/opnfv_testapi/ui/auth/sign.py
@@ -1,13 +1,11 @@
 from six.moves.urllib import parse
 from tornado import gen
 from tornado import web
-import logging
 
 from opnfv_testapi.common.config import CONF
+from opnfv_testapi.db import api as dbapi
 from opnfv_testapi.ui.auth import base
 from opnfv_testapi.ui.auth import constants as const
-
-# CONF = config.Config()
 
 
 class SigninHandler(base.BaseHandler):
@@ -48,10 +46,9 @@ class SigninReturnHandler(base.BaseHandler):
             'fullname': self.get_query_argument(const.OPENID_NS_SREG_FULLNAME),
             const.ROLE: role
         }
-        user = yield self.db_find_one({'openid': openid})
+        user = yield dbapi.db_find_one(self.table, {'openid': openid})
         if not user:
-            self.db_save(self.table, new_user_info)
-            logging.info('save to db:%s', new_user_info)
+            dbapi.db_save(self.table, new_user_info)
         else:
             role = user.get(const.ROLE)
 
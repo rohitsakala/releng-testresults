f0fdac6e33287030c70b223301f18f5bf9060de3
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-01-10 12:01:59 +0800

    reporting leveraging the new repo releng-testresults
    Change-Id: I8dd00f3ef996c8847d803b690423bf381676e49d
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- reporting/docker/web_server.sh
+++ reporting/docker/web_server.sh
@@ -3,11 +3,11 @@ cp -r display /usr/share/nginx/html
 
 
 # nginx config
-cp /home/opnfv/releng/utils/test/reporting/docker/nginx.conf /etc/nginx/conf.d/
+cp /home/opnfv/releng-testresults/reporting/docker/nginx.conf /etc/nginx/conf.d/
 echo "daemon off;" >> /etc/nginx/nginx.conf
 
 # supervisor config
-cp /home/opnfv/releng/utils/test/reporting/docker/supervisor.conf /etc/supervisor/conf.d/
+cp /home/opnfv/releng-testresults/reporting/docker/supervisor.conf /etc/supervisor/conf.d/
 
 # Manage Angular front end
 cd pages && /bin/bash angular.sh

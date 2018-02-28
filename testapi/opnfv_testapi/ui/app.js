788d670037a77b35148551ebecf1314146e5a4b7
thuva4 <tharma.thuva@gmail.com>
2018-02-28 21:56:23 +0530

    Add enter to search
    Add directive to filter the list by enter
    
    JIRA: RELENG-351
    
    Change-Id: I1abc92cdf2cb103e1ba9b685437b65035c88dd5d
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- testapi/opnfv_testapi/ui/app.js
+++ testapi/opnfv_testapi/ui/app.js
@@ -42,6 +42,21 @@
             };
         }]);
 
+    angular
+        .module('testapiApp')
+        .directive('ngEnter', function () {
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if(event.which === 13) {
+                        scope.$apply(function (){
+                            scope.$eval(attrs.ngEnter);
+                        });
+                        event.preventDefault();
+                    }
+                });
+            };
+        });
+
     configureRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     /**
